8aeb2f69a7b526ae7f5688e705768e2b7b11fe92
Alok prasad <118293432+Alok-2004@users.noreply.github.com>
2023-08-20 17:35:37 +0530

    Using binary sort

--- 34_firstAndLastIndex.cpp
+++ 34_firstAndLastIndex.cpp
@@ -2,7 +2,8 @@
 #include<vector>
 
 using namespace std;
-
+/*
+Time complexity is O(n)
 vector<int> lastAndFirst(vector<int> &nums, int target)
 {
 	int n = nums.size();
@@ -30,6 +31,53 @@ vector<int> lastAndFirst(vector<int> &nums, int target)
 	}
 	return result;
 }
+*/
+
+vector<int> searchRange(vector<int>& nums, int target) 
+{
+       int n = nums.size();
+       vector<int> result(2, -1);
+        
+        // For first position
+        int lo = 0;
+        int hi = n - 1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+            {
+                if (mid == 0 || nums[mid - 1] != target)
+                {
+                    result[0] = mid;
+                    break;
+                }
+                else hi = mid - 1;
+            }
+            else if (nums[mid] > target) hi = mid - 1;
+            else  lo = mid + 1;
+        }
+
+        // For last position
+        int lo1 = 0;
+        int hi1 = n - 1;
+
+        while (lo1 <= hi1)
+        {
+            int mid = lo1 + (hi1 - lo1) / 2;
+            if (nums[mid] == target)
+            {
+                if (mid == n - 1 || nums[mid + 1] != target)
+                {
+                    result[1] = mid;
+                    break;
+                }
+                else lo1 = mid + 1;
+            }
+            else if (nums[mid] > target)  hi1 = mid - 1;
+            else  lo1 = mid + 1;
+        }
+        return result;
+    }
 int main()
 {
 	vector<int> nums;
